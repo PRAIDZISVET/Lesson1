10332f86b783673eae5655d551a92366f5b26bc5
PRAIDZISVET <86moiseev@gmail.com>
2021-01-25 18:33:20 +0300

    Added Lesson1

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.itacademy.lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        char ch = '\u0066';
+        System.out.println(ch);
+    }
+}
